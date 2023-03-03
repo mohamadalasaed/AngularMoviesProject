b7d8e9eadea5930bdeee9d86e913b1a0da6e7e5f
= <=>
2023-03-03 12:31:32 +0200

    Created home-header component to search for a movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeHeaderComponent } from './home-header/home-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeHeaderComponent
   ],
   imports: [
     BrowserModule,
